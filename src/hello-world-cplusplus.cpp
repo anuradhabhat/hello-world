76591fdab0e8f583b4f0d81fa6fe5fb530bad7ed
abhat <bhat.anuradha@gmail.com>
2018-01-30 12:47:48 -0500

    cpp

---
+++ src/hello-world-cplusplus.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : hello-world-cplusplus.cpp
+// Author      : abhat
+// Version     :
+// Copyright   : abhat's copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
